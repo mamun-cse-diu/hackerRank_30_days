1e6615039fca30bbc775db9f3923ca21ee0e6450
MD.AL-MAMUN <59719431+mamun-cse-diu@users.noreply.github.com>
2020-06-15 11:25:52 +0600

    Add files via upload

---
+++ Day 0.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char s[50];
+    gets(s);
+    printf("Hello, World.\n");
+    printf("%s\n",s);
+    return 0;
+}
---
+++ Day 1.c
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    string ss;
+    int n;
+    double dd;
+    cin>>n;
+    cin>>dd;
+    getline(cin, ss);
+    cout<<(n+i)<<endl;
+    cout<<(dd+d)<<endl;
+    cout<<s;
+    cout<<ss<<endl;
+
+
+    return 0;
+}
